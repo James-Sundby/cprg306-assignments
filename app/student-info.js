bb14646da7841ce26e2d9e598387d373e7ca8bc2
James-Sundby <119804969+James-Sundby@users.noreply.github.com>
2023-09-12 14:07:49 -0600

    Adds week 2 assignment

---
+++ app/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <>
+            <p>Name: James Sundby</p>
+            <p>Course Section: CPRG 306 A</p> 
+            <Link href="https://github.com/James-Sundby">https://github.com/James-Sundby</Link>       
+        </>
+    )
+}
---
+++ app/week2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "../student-info";
+
+export default function Home() {
+    return (
+      <>
+        <h1>My Shopping List</h1>
+        <StudentInfo />
+      </>
+    )
+  }
