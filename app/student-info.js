4230b4096aaa6fe5a348f5103a88656a3986ade3
James-Sundby <119804969+James-Sundby@users.noreply.github.com>
2023-09-13 08:52:16 -0600

    Adds indentation to studentinfo.js

--- app/student-info.js
+++ app/student-info.js
@@ -5,7 +5,9 @@ export default function StudentInfo() {
         <>
             <p>Name: James Sundby</p>
             <p>Course Section: CPRG 306 A</p> 
-            <Link href="https://github.com/James-Sundby">https://github.com/James-Sundby</Link>       
+            <p>
+                <Link href="https://github.com/James-Sundby">https://github.com/James-Sundby</Link> 
+            </p>      
         </>
     )
 }
