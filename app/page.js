d541744e8e481c5548740f41e52267bb3b490acc
James-Sundby <119804969+James-Sundby@users.noreply.github.com>
2023-09-15 12:41:34 -0600

    Adds Tailwind classes from in-class demo

--- app/page.js
+++ app/page.js
@@ -3,10 +3,10 @@ import StudentInfo from "./student-info";
 
 export default function Home() {
   return (
-    <>
-      <h1 className="">CPRG 306: Web Development 2 - Assignments</h1>
+    <main>
+      <h1 className="text-2xl m-4">CPRG 306: Web Development 2 - Assignments</h1>
       <StudentInfo />
       <Link href="/week2">Week 2</Link>
-    </>
+    </main>
   )
 }
