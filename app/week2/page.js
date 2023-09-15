df52926c57dd322d3177a7c4ca518200ab931ce0
James-Sundby <119804969+James-Sundby@users.noreply.github.com>
2023-09-15 13:29:23 -0600

    Adds demo css to page
    Adds the demo css to the week 2 page

--- app/week2/page.js
+++ app/week2/page.js
@@ -3,7 +3,7 @@ import StudentInfo from "../student-info";
 export default function Home() {
     return (
       <>
-        <h1>My Shopping List</h1>
+        <h1 className="text-2xl m-4">My Shopping List</h1>
         <StudentInfo />
       </>
     )
