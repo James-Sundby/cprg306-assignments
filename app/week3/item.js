96e472d69493f8e638fb334824d295df51f1b3e5
James-Sundby <119804969+James-Sundby@users.noreply.github.com>
2023-09-22 11:47:15 -0600

    Changes centering for list items

--- app/week3/item.js
+++ app/week3/item.js
@@ -1,7 +1,7 @@
 export default function Item({ name, quantity, category }) {
     return (
         <li className="bg-gray-600 p-2 m-2 rounded flex w-1/3">
-            <div className="w-full pr-4 border-r border-gray-400 text-center">
+            <div className="w-full pr-4 border-r border-gray-400 flex items-center">
                 <h2 className="font-bold text-2xl">{name}</h2>
             </div>
             <div className="w-1/2 pl-4">
@@ -11,3 +11,7 @@ export default function Item({ name, quantity, category }) {
       </li>
     )
 }
+
+/* <div className="w-full pr-4 border-r border-gray-400 flex items-center justify-center">
+    Could be used to center the text horizontally inside the div, not sure if it's needed.
+*/
