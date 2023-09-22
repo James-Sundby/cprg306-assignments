851a91ebedd3af79ec4f2ac987cb4217d11bb346
James-Sundby <119804969+James-Sundby@users.noreply.github.com>
2023-09-22 13:44:02 -0600

    Sets width to full

--- app/week3/item.js
+++ app/week3/item.js
@@ -1,6 +1,6 @@
 export default function Item({ name, quantity, category }) {
     return (
-        <li className="bg-gray-600 p-2 m-2 rounded flex w-1/3 sm:w-fill border-2 border-gray-900 hover:border-white">
+        <li className="bg-gray-600 p-2 m-2 rounded flex w-full max-w-md sm:w-fill border-2 border-gray-900 hover:border-white">
             <div className="w-full pr-4 border-r border-gray-400 flex items-center">
                 <h2 className="font-bold text-2xl">{name}</h2>
             </div>
