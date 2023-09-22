be5a3468ea5d9cb4f17a62dca57a1b47a139c34f
James-Sundby <119804969+James-Sundby@users.noreply.github.com>
2023-09-22 13:25:26 -0600

    Changes max to fill

--- app/week3/item.js
+++ app/week3/item.js
@@ -1,8 +1,8 @@
 export default function Item({ name, quantity, category }) {
     return (
-        <li className="bg-gray-600 p-2 m-2 rounded flex w-1/3 sm:w-max border-2 border-gray-900 hover:border-white">
+        <li className="bg-gray-600 p-2 m-2 rounded flex w-1/3 sm:w-fill border-2 border-gray-900 hover:border-white">
             <div className="w-full pr-4 border-r border-gray-400 flex items-center">
-                <h2 className="font-bold text-2xl hover:tx">{name}</h2>
+                <h2 className="font-bold text-2xl">{name}</h2>
             </div>
             <div className="w-1/2 pl-4 hover:font-bold">
                 <p>Qty: {quantity}</p>
