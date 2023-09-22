43ef682496ae44ff99b42c36ae155133063aab55
James-Sundby <119804969+James-Sundby@users.noreply.github.com>
2023-09-22 12:55:54 -0600

    Adds indents for readability

--- app/week3/page.js
+++ app/week3/page.js
@@ -3,8 +3,8 @@ import ItemList from "./item-list"
 export default function Home() {
     return (
         <main>
-        <h1 className="text-4xl m-4 font-bold">Shopping List</h1>
-        <ItemList />
+            <h1 className="text-4xl m-4 font-bold">Shopping List</h1>
+            <ItemList />
         </main>
     )
 }
