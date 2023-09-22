86f05dcd12935a570e2893d9f3b717961f9843b9
James-Sundby <119804969+James-Sundby@users.noreply.github.com>
2023-09-22 13:10:18 -0600

    Adds hover select styling

--- app/week3/item.js
+++ app/week3/item.js
@@ -1,10 +1,10 @@
 export default function Item({ name, quantity, category }) {
     return (
-        <li className="bg-gray-600 p-2 m-2 rounded flex w-1/3">
+        <li className="bg-gray-600 p-2 m-2 rounded flex w-1/3 border-2 border-gray-900 hover:border-white">
             <div className="w-full pr-4 border-r border-gray-400 flex items-center">
                 <h2 className="font-bold text-2xl">{name}</h2>
             </div>
-            <div className="w-1/2 pl-4">
+            <div className="w-1/2 pl-4 hover:font-bold">
                 <p>Qty: {quantity}</p>
                 <p>In {category}</p>
             </div>
