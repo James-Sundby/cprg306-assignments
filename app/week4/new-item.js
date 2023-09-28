23eaa810c819474eb7e7579981c9815d02a7ab07
James-Sundby <119804969+James-Sundby@users.noreply.github.com>
2023-09-28 07:53:50 -0600

    Updates look with copilot suggestions

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -34,108 +34,59 @@ export default function NewItem(){
 
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <span>Name: </span>
+            <h1 className = "text-2xl m-4">Add Item</h1>
+            <form onSubmit = {handleSubmit}>
+                <div className = "flex flex-col m-4">
+                    <label className="text-xl">Name: </label>
                     <input 
-                        type="text"
+                        type = "text"
                         required 
-                        onChange={handleNameChange} 
-                        value={name} 
-                        className=""
+                        onChange = {handleNameChange} 
+                        value = {name} 
+                        className ="border-2 border-gray-900 rounded p-2 text-black"
                     />
-                </label>
-                <label>
-                    <span>Quantity: </span>
+                </div>
+                <div className = "flex flex-col m-4">
+                    <label className = "text-xl">Quantity: </label>
                     <input 
-                        type="number"
-                        min="1"
-                        max="99"
+                        type = "number"
+                        min = "1"
+                        max = "99"
                         required 
-                        onChange={handleQuantityChange} 
-                        value={quantity} 
-                        className=""
+                        onChange = {handleQuantityChange} 
+                        value = {quantity} 
+                        className = "border-2 border-gray-900 rounded p-2 text-black"
                     />
-                </label>
-                <label>
-                    <span>Category: </span>
+                </div>
+                <div className = "flex flex-col m-4">
+                    <label className = "text-xl">Category: </label>
                     <select 
                         required 
-                        onChange={handleCategoryChange} 
-                        value={category} 
-                        className=""
+                        onChange = {handleCategoryChange} 
+                        value = {category} 
+                        className = "border-2 border-gray-900 rounded p-2 text-black"
                     >
-                        <option value="produce">Produce</option>
-                        <option value="dairy">Dairy</option>
-                        <option value="bakery">Bakery</option>
-                        <option value="meat">Meat</option>
-                        <option value="frozen foods">Frozen Foods</option>
-                        <option value="canned goods">Canned Goods</option>
-                        <option value="dry goods">Dry Goods</option>
-                        <option value="beverages">Beverages</option>
-                        <option value="snacks">Snacks</option>
-                        <option value="household">Household</option>
-                        <option value="other">Other</option>
+                        <option value = "produce">Produce</option>
+                        <option value = "dairy">Dairy</option>
+                        <option value = "bakery">Bakery</option>
+                        <option value = "meat">Meat</option>
+                        <option value = "frozen foods">Frozen Foods</option>
+                        <option value = "canned goods">Canned Goods</option>
+                        <option value = "dry goods">Dry Goods</option>
+                        <option value = "beverages">Beverages</option>
+                        <option value = "snacks">Snacks</option>
+                        <option value = "household">Household</option>
+                        <option value = "other">Other</option>
                     </select>
-                </label>
-                <button 
-                    type="submit"
-                    className="">
-                    Add Item
-                </button>
+                </div>
+                <div className = "flex flex-col m-4">
+                    <button
+                        type = "submit"
+                        className = "bg-gray-900 text-white rounded p-2">
+                        Add Item
+                    </button>
+                </div>
             </form>
         </div>
     );
 }
-
-
-
-/* Github copilot generated code suggestions
-    export default function NewItem({ onAdd }) {
-    const [name, setName] = useState("");
-    const [quantity, setQuantity] = useState(0);
-    const [category, setCategory] = useState("");
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        onAdd({ name, quantity, category });
-    };
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <h1 className="text-2xl m-4">Add New Item</h1>
-            <div className="flex flex-col m-4">
-                <label className="text-xl">Name</label>
-                <input
-                    className="border-2 border-gray-900 rounded p-2"
-                    type="text"
-                    value={name}
-                    onChange={(event) => setName(event.target.value)}
-                />
-            </div>
-            <div className="flex flex-col m-4">
-                <label className="text-xl">Quantity</label>
-                <input
-                    className="border-2 border-gray-900 rounded p-2"
-                    type="number"
-                    value={quantity}
-                    onChange={(event) => setQuantity(event.target.value)}
-                />
-            </div>
-            <div className="flex flex-col m-4">
-                <label className="text-xl">Category</label>
-                <input
-                    className="border-2 border-gray-900 rounded p-2"
-                    type="text"
-                    value={category}
-                    onChange={(event) => setCategory(event.target.value)}
-                />
-            </div>
-            <div className="flex flex-col m-4">
-                <button className="bg-gray-900 text-white rounded p-2" type="submit">
-                    Add Item
-                </button>
-            </div>
-        </form>
-    );
-}*/
