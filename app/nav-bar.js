83b1fadb5ecad3a316a1af3bc229ee651d18c63e
James-Sundby <119804969+James-Sundby@users.noreply.github.com>
2023-09-28 10:21:51 -0600

    Adds some tailwind styling to NavBar

--- app/nav-bar.js
+++ app/nav-bar.js
@@ -3,11 +3,11 @@ import Link from "next/link";
 export default function NavBar() {
     return (
         <>
-            <div className = "flex">
+            <div className = "flex justify-between">
                 <div className = "font-bold mx-2">
                     <h1>CPRG 306: Web Development 2 - Assignment</h1>
                 </div>
-                <div className = "font-bold mx-2">
+                <div className = "font-bold mx-2 bg-gray-600 rounded p-2">
                     <p>
                         <Link href="/">Home</Link> 
                     </p>
