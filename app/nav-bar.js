8769ebd66a172d174a433dd5c30b8f31500b37c6
James-Sundby <119804969+James-Sundby@users.noreply.github.com>
2023-09-29 13:19:02 -0600

    Centers item header on wk4

--- app/nav-bar.js
+++ app/nav-bar.js
@@ -3,15 +3,18 @@ import Link from "next/link";
 export default function NavBar() {
     return (
         <>
-            <div className = "flex justify-between">
+            <div className = "flex justify-between p-2 ">
                 <div className = "font-bold mx-2">
                     <h1>CPRG 306: Web Development 2 - Assignment</h1>
                 </div>
-                <div className = "font-bold mx-2 bg-gray-600 rounded p-2">
-                    <p>
-                        <Link href="/">Home</Link> 
-                    </p>
-                </div>   
+                <div className="flex">
+                    <div className = "font-bold mx-2 bg-gray-600 rounded px-2 hover:bg-gray-700">
+                            <Link href="/">Home</Link> 
+                    </div> 
+                    <div className = "font-bold mx-2 bg-gray-600 rounded px-2 hover:bg-gray-700">
+                            <Link href="https://github.com/James-Sundby">GitHub</Link> 
+                    </div>
+                </div>
             </div>  
         </>
     )
--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -13,7 +13,7 @@ export default function NewItem(){
         const newItem = { name, quantity, category };
         console.log(newItem);
 
-        alert(`Name: ${name}\nQuantity: ${quantity}\nCategory: ${category}`);
+        alert(`Name: ${name}, Quantity: ${quantity}, Category: ${category}`);
 
         setName("");
         setQuantity(1);
@@ -33,63 +33,65 @@ export default function NewItem(){
     };
 
     return (
-        <div className = "flex flex-col w-full max-w-lg sm:w-fill bg-gray-600 p-2 m-2 rounded">
-            <h1 className = "text-2xl m-4">Add Item</h1>
-            <form onSubmit = {handleSubmit}>
-                <div className = "flex flex-col m-4">
-                    <label className="text-xl">Name: </label>
-                    <input 
-                        placeholder = "Item Name"
-                        type = "text"
-                        required 
-                        onChange = {handleNameChange} 
-                        value = {name} 
-                        className ="border-2 border-gray-900 rounded p-2 text-black"
-                    />
-                </div>
-                <div className = "flex justify-between ">
+        <main className="flex justify-center">
+            <div className = "flex flex-col w-full max-w-lg sm:w-fill bg-gray-600 p-2 m-2 rounded justify-center">
+                <h1 className = "text-4xl text-center">Add New Item</h1>
+                <form onSubmit = {handleSubmit}>
                     <div className = "flex flex-col m-4">
-                        <label className = "text-xl">Quantity: </label>
+                        <label className="text-xl">Name: </label>
                         <input 
-                            type = "number"
-                            min = "1"
-                            max = "99"
+                            placeholder = "Item Name"
+                            type = "text"
                             required 
-                            onChange = {handleQuantityChange} 
-                            value = {quantity} 
-                            className = "border-2 border-gray-900 rounded p-2 text-black"
+                            onChange = {handleNameChange} 
+                            value = {name} 
+                            className ="border-2 border-gray-900 rounded p-2 text-black"
                         />
                     </div>
-                    <div className = "flex flex-col m-4">
-                        <label className = "text-xl">Category: </label>
-                        <select 
-                            required 
-                            onChange = {handleCategoryChange} 
-                            value = {category} 
-                            className = "border-2 border-gray-900 rounded p-2 text-black"
-                        >
-                            <option value = "produce">Produce</option>
-                            <option value = "dairy">Dairy</option>
-                            <option value = "bakery">Bakery</option>
-                            <option value = "meat">Meat</option>
-                            <option value = "frozen foods">Frozen Foods</option>
-                            <option value = "canned goods">Canned Goods</option>
-                            <option value = "dry goods">Dry Goods</option>
-                            <option value = "beverages">Beverages</option>
-                            <option value = "snacks">Snacks</option>
-                            <option value = "household">Household</option>
-                            <option value = "other">Other</option>
-                        </select>
+                    <div className = "flex justify-between ">
+                        <div className = "flex flex-col m-4">
+                            <label className = "text-xl">Quantity: </label>
+                            <input 
+                                type = "number"
+                                min = "1"
+                                max = "99"
+                                required 
+                                onChange = {handleQuantityChange} 
+                                value = {quantity} 
+                                className = "border-2 border-gray-900 rounded p-2 text-black"
+                            />
+                        </div>
+                        <div className = "flex flex-col m-4">
+                            <label className = "text-xl">Category: </label>
+                            <select 
+                                required 
+                                onChange = {handleCategoryChange} 
+                                value = {category} 
+                                className = "border-2 border-gray-900 rounded p-2 text-black"
+                            >
+                                <option value = "produce">Produce</option>
+                                <option value = "dairy">Dairy</option>
+                                <option value = "bakery">Bakery</option>
+                                <option value = "meat">Meat</option>
+                                <option value = "frozen foods">Frozen Foods</option>
+                                <option value = "canned goods">Canned Goods</option>
+                                <option value = "dry goods">Dry Goods</option>
+                                <option value = "beverages">Beverages</option>
+                                <option value = "snacks">Snacks</option>
+                                <option value = "household">Household</option>
+                                <option value = "other">Other</option>
+                            </select>
+                        </div>
                     </div>
-                </div>
-                <div className = "flex flex-col m-4">
-                    <button
-                        type = "submit"
-                        className = "bg-gray-900 text-white rounded p-2">
-                        Add Item
-                    </button>
-                </div>
-            </form>
-        </div>
+                    <div className = "flex flex-col m-4">
+                        <button
+                            type = "submit"
+                            className = "bg-gray-900 text-white rounded p-2">
+                            Save
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </main>
     );
 }
