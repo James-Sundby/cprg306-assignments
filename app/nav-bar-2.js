d4b8e4c0d720efbbce19b7d43c0b692ed39739e4
James-Sundby <119804969+James-Sundby@users.noreply.github.com>
2023-10-15 14:22:46 -0600

    Adds to colour theme picker

--- app/nav-bar-2.js
+++ app/nav-bar-2.js
@@ -90,33 +90,34 @@ export default function NavBar() {
           <label tabIndex="0" className="btn btn-ghost btn-square">
             <svg
               xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 512 512"
               className="h-5 w-5 stroke-secondary fill-secondary"
-              viewBox="0 0 24 24"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
+              <path d="M0 64C0 28.7 28.7 0 64 0H352c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zM160 352c0-17.7 14.3-32 32-32V304c0-44.2 35.8-80 80-80H416c17.7 0 32-14.3 32-32V160 69.5c37.3 13.2 64 48.7 64 90.5v32c0 53-43 96-96 96H272c-8.8 0-16 7.2-16 16v16c17.7 0 32 14.3 32 32V480c0 17.7-14.3 32-32 32H192c-17.7 0-32-14.3-32-32V352z" />
             </svg>
           </label>
           <ul
             tabIndex={0}
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52 right-0 mr-2"
           >
-            <li className="disabled">Choose a colour option</li>
             <button className="btn btn-sm btn-ghost" data-set-theme="dracula">
               Default
             </button>
             <button className="btn btn-sm btn-ghost" data-set-theme="cyberpunk">
               Cyberpunk
             </button>
-            <button className="btn btn-sm btn-ghost" data-set-theme="">
+            <button className="btn btn-sm btn-ghost" data-set-theme="dark">
               Dark
             </button>
             <button className="btn btn-sm btn-ghost" data-set-theme="cupcake">
               Light
+            </button>
+            <button className="btn btn-sm btn-ghost" data-set-theme="night">
+              Dark Blue
+            </button>
+            <button className="btn btn-sm btn-ghost" data-set-theme="forest">
+              Green
             </button>
           </ul>
         </div>
