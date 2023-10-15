f6eb7df91a01f2928654eb66b60bc6bf1705d323
James-Sundby <119804969+James-Sundby@users.noreply.github.com>
2023-10-15 12:48:13 -0600

    Updates item, adds remove placeholder

--- app/week6/item.js
+++ app/week6/item.js
@@ -1,12 +1,19 @@
 export default function Item({ name, quantity, category }) {
   return (
-    <li className="bg-gray-600 p-2 m-2 rounded-lg flex w-full max-w-lg sm:w-fill border-2 border-gray-900 hover:border-white hover:font-bold">
-      <div className="w-full pr-4 border-r border-gray-400 flex items-center">
-        <p className="font-bold text-2xl">{name}</p>
-      </div>
-      <div className="w-1/2 pl-4">
-        <p>Qty: {quantity}</p>
-        <p>In {category}</p>
+    <li>
+      <div className="card bg-base-200 shadow-xl max-w-lg ml-2 mb-2">
+        <div className="card-body flex-row justify-between">
+          <div>
+            <h2 className="card-title text-2xl">{name}</h2>
+            <p>
+              Pick up <span className="font-bold text-primary">{quantity}</span>{" "}
+              in <span className="font-bold text-primary">{category}</span>
+            </p>
+          </div>
+          <div className="card-actions">
+            <button className="btn btn-primary">Remove</button>
+          </div>
+        </div>
       </div>
     </li>
   );
