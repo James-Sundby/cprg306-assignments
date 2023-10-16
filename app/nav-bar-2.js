b20a1140b598d3796e9f68e4396dd08dbd6436c6
James-Sundby <119804969+James-Sundby@users.noreply.github.com>
2023-10-15 20:06:51 -0600

    Update nav-bar-2.js

--- app/nav-bar-2.js
+++ app/nav-bar-2.js
@@ -29,7 +29,7 @@ export default function NavBar() {
           </label>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
+            className="menu menu-sm dropdown-content mt-3 z-[2] p-2 shadow bg-base-200 rounded-box w-52"
           >
             <li className="disabled">Assignments</li>
             <li>
