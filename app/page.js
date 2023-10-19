cbf5e92dffe14ddb75b0ee410f46df869236558b
James-Sundby <119804969+James-Sundby@users.noreply.github.com>
2023-10-19 10:14:58 -0600

    Adds update to formatting

--- app/page.js
+++ app/page.js
@@ -7,27 +7,29 @@ export default function Home() {
       <h1 className="text-2xl m-4">
         CPRG 306: Web Development 2 - Assignments
       </h1>
-      <StudentInfo />
-      <ul>
-        <li>
-          <Link href="/week2">Week 2</Link>
-        </li>
-        <li>
-          <Link href="/week3">Week 3</Link>
-        </li>
-        <li>
-          <Link href="/week4">Week 4</Link>
-        </li>
-        <li>
-          <Link href="/week5">Week 5</Link>
-        </li>
-        <li>
-          <Link href="/week6">Week 6</Link>
-        </li>
-        <li>
-          <Link href="/week7">Week 7</Link>
-        </li>
-      </ul>
+      <div className="mx-2">
+        <StudentInfo />
+        <ul>
+          <li>
+            <Link href="/week2">Week 2</Link>
+          </li>
+          <li>
+            <Link href="/week3">Week 3</Link>
+          </li>
+          <li>
+            <Link href="/week4">Week 4</Link>
+          </li>
+          <li>
+            <Link href="/week5">Week 5</Link>
+          </li>
+          <li>
+            <Link href="/week6">Week 6</Link>
+          </li>
+          <li>
+            <Link href="/week7">Week 7</Link>
+          </li>
+        </ul>
+      </div>
     </main>
   );
 }
--- app/week2/page.js
+++ app/week2/page.js
@@ -1,14 +1,16 @@
 import StudentInfo from "../student-info";
-import NavBar from "../nav-bar"
+import NavBar from "../nav-bar";
 
 export default function Home() {
-    return (
-      <>
-        <NavBar />
-        <main>
-          <h1 className="text-2xl m-4">My Shopping List</h1>
+  return (
+    <>
+      <NavBar />
+      <main>
+        <h1 className="text-2xl m-4">My Shopping List</h1>
+        <div className="mx-2">
           <StudentInfo />
-        </main>
-      </>
-    )
+        </div>
+      </main>
+    </>
+  );
 }
