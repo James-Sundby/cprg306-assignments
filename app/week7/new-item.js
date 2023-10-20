fab46dc9541f0e9039cd74f644e3f972d8212b54
James-Sundby <119804969+James-Sundby@users.noreply.github.com>
2023-10-20 07:20:55 -0600

    Changed add item to modal display
    Trying to find a good way to fit the meal-ideas on a small screen

--- app/week7/new-item.js
+++ app/week7/new-item.js
@@ -31,52 +31,72 @@ export default function NewItem({ onAddItem }) {
   };
 
   return (
-    <div className="card bg-base-200 max-w-lg mx-2 mb-2">
-      <form className="card-body" onSubmit={handleSubmit}>
-        <input
-          placeholder="Item Name"
-          type="text"
-          required
-          onChange={handleNameChange}
-          value={name}
-          className="input input-bordered"
-        />
-        <div className="flex space-x-2">
-          <input
-            type="number"
-            min="1"
-            max="99"
-            required
-            onChange={handleQuantityChange}
-            value={quantity}
-            className="input input-bordered w-1/4"
-          />
-          <select
-            required
-            onChange={handleCategoryChange}
-            value={category}
-            className="select select-bordered flex-grow"
-          >
-            <option disabled>Select a Category</option>
-            <option value="produce">Produce</option>
-            <option value="dairy">Dairy</option>
-            <option value="bakery">Bakery</option>
-            <option value="meat">Meat</option>
-            <option value="frozen foods">Frozen Foods</option>
-            <option value="canned goods">Canned Goods</option>
-            <option value="dry goods">Dry Goods</option>
-            <option value="beverages">Beverages</option>
-            <option value="snacks">Snacks</option>
-            <option value="household">Household</option>
-            <option value="other">Other</option>
-          </select>
+    <>
+      <div className="flex max-w-lg mx-2 mb-2">
+        <button
+          className="flex-1 btn btn-primary"
+          onClick={() => document.getElementById("my_modal_1").showModal()}
+        >
+          Add Item to List
+        </button>
+      </div>
+      <dialog id="my_modal_1" className="modal">
+        <div className=" bg-base-200 max-w-lg mx-2 mb-2 modal-box">
+          <form className="card-body" onSubmit={handleSubmit} method="dialog">
+            <input
+              placeholder="Item Name"
+              type="text"
+              required
+              onChange={handleNameChange}
+              value={name}
+              className="input input-bordered"
+            />
+            <div className="flex space-x-2">
+              <input
+                type="number"
+                min="1"
+                max="99"
+                required
+                onChange={handleQuantityChange}
+                value={quantity}
+                className="input input-bordered w-1/4"
+              />
+              <select
+                required
+                onChange={handleCategoryChange}
+                value={category}
+                className="select select-bordered flex-grow"
+              >
+                <option disabled>Select a Category</option>
+                <option value="produce">Produce</option>
+                <option value="dairy">Dairy</option>
+                <option value="bakery">Bakery</option>
+                <option value="meat">Meat</option>
+                <option value="frozen foods">Frozen Foods</option>
+                <option value="canned goods">Canned Goods</option>
+                <option value="dry goods">Dry Goods</option>
+                <option value="beverages">Beverages</option>
+                <option value="snacks">Snacks</option>
+                <option value="household">Household</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
+            <div className="flex gap-2">
+              <button type="submit" className="btn btn-primary flex-1">
+                Add Item
+              </button>
+              <button
+                className="btn btn-primary flex-1"
+                onClick={() => {
+                  document.getElementById("my_modal_1").close();
+                }}
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
         </div>
-        <div className="flex flex-col">
-          <button type="submit" className="btn btn-primary">
-            Add Item
-          </button>
-        </div>
-      </form>
-    </div>
+      </dialog>
+    </>
   );
 }
