9f4735f37154421cfa79107d2a8484265051c3e5
James-Sundby <119804969+James-Sundby@users.noreply.github.com>
2023-10-24 17:30:11 -0600

    adds wk8 functionality

--- app/week8/shopping-list/page.js
+++ app/week8/shopping-list/page.js
@@ -1,14 +1,17 @@
 "use client";
 
 import { useState } from "react";
+import { useUserAuth } from "../_utils/auth-context";
 import ItemList from "./item-list";
 import NewItem from "./new-item";
 import itemsData from "./items.json";
 import MealIdeas from "./meal-ideas";
+import Link from "next/link";
 
-import NavBar from "../nav-bar-2";
+import NavBar from "../../nav-bar-2";
 
 export default function Home() {
+  const { user } = useUserAuth();
   const [items, setItems] = useState(itemsData);
   const [selectedItemName, setSelectedItemName] = useState("");
   const [numberOfMeals, setNumberOfMeals] = useState(0);
@@ -33,30 +36,54 @@ export default function Home() {
   return (
     <>
       <NavBar />
-      <main>
-        <h1 className="text-4xl m-4 font-bold ">Shopping List</h1>
-        <div className="carousel w-screen">
-          <div className="carousel-item lg:max-wd-lg">
-            <div className="carousel-content-wrapper w-screen lg:w-auto">
-              <NewItem onAddItem={handleAddItem} />
-              <ItemList
-                items={items}
-                onDelete={handleRemoveItem}
-                onItemSelect={handleItemSelect}
-                numberOfMeals={numberOfMeals}
-              />
+      <h1 className="text-4xl m-4 font-bold ">Shopping List</h1>
+      {user ? (
+        <main>
+          <div className="carousel w-screen">
+            <div className="carousel-item lg:max-wd-lg">
+              <div className="carousel-content-wrapper w-screen lg:w-auto">
+                <NewItem onAddItem={handleAddItem} />
+                <ItemList
+                  items={items}
+                  onDelete={handleRemoveItem}
+                  onItemSelect={handleItemSelect}
+                  numberOfMeals={numberOfMeals}
+                />
+              </div>
+            </div>
+            <div className="carousel-item lg:max-wd-lg">
+              <div className="carousel-content-wrapper w-screen lg:w-auto">
+                <MealIdeas
+                  ingredient={selectedItemName}
+                  updateNumberOfMeals={setNumberOfMeals}
+                />
+              </div>
             </div>
           </div>
-          <div className="carousel-item lg:max-wd-lg">
-            <div className="carousel-content-wrapper w-screen lg:w-auto">
-              <MealIdeas
-                ingredient={selectedItemName}
-                updateNumberOfMeals={setNumberOfMeals}
-              />
+        </main>
+      ) : (
+        <div className="card bg-base-200 shadow-xl max-w-lg mx-2 mb-2">
+          <div className="card-body">
+            <div>
+              <h2 className="card-title text-2xl">Sorry</h2>
+              <p>You must be logged in to access this page</p>
+            </div>
+            <div className="card-actions justify-center">
+              <Link className="btn btn-primary btn-wide" href="/week8">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="1em"
+                  viewBox="0 0 576 512"
+                  className="inline-block w-5 h-5 stroke-current fill-primary-content"
+                >
+                  <path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c.2 35.5-28.5 64.3-64 64.3H128.1c-35.3 0-64-28.7-64-64V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24zM352 224a64 64 0 1 0 -128 0 64 64 0 1 0 128 0zm-96 96c-44.2 0-80 35.8-80 80c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16c0-44.2-35.8-80-80-80H256z" />
+                </svg>
+                Return to Home
+              </Link>
             </div>
           </div>
         </div>
-      </main>
+      )}
     </>
   );
 }
